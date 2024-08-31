104de3b89b4b31b88702a87124a58b8017735786
tiago863 <tiagopedro02.iscte@gmail.com>
2024-08-31 12:37:29 +0100

    update final do portofolio

--- src/components/FeaturedBox/FeaturedBox.tsx
+++ src/components/FeaturedBox/FeaturedBox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import Typed from "typed.js";
 import avatar from "../../assets/images/perfil_image.png";
 
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
-const Layout = ({ children }) => {
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
